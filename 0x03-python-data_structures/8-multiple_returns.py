39b01d6f18a5e8bf0065c9ad1f7ac180b972625e
tony <ebunifeoluwaadenuga@gmail.com>
2023-03-14 02:38:52 +0000

    aaa

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    if length != 0:
-        first = sentence[0]
-    else:
-        first = None
-    return (length, first)
+    return (len(sentence), sentence[0] if sentence != "" else None)
--- 0x03-python-data_structures/list.h
+++ 0x03-python-data_structures/list.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - singly linked list
@@ -14,13 +15,16 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
+
 #endif /* LISTS_H */
+
