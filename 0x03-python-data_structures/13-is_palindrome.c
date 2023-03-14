cff54b52ab9ea58c56750ca1db2d3263a8452cd5
tony <ebunifeoluwaadenuga@gmail.com>
2023-03-14 02:44:34 +0000

    aaa

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,73 +1,43 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: Head pointer to the first list
+ * Return: 0 if it is not a palindrom,
+ *		   1 if it is a palindrome
  */
-void reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
 
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
-}
-
-/**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- *
- * Return: 1 if it is, 0 if not
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *next_node = *head;
+	listint_t *current_node = *head;
+	listint_t *ptr = *head;
+	listint_t *prev_node = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
+	while (next_node != NULL)
+	{
+		next_node = next_node->next;
+		current_node->next = prev_node;
+		prev_node = current_node;
+		current_node = next_node;
+	}
+	*head = prev_node;
+
+	if (ptr == NULL || ptr->next == NULL)
 		return (1);
 
-	while (1)
+	while (ptr->next != NULL)
 	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
+		ptr = ptr->next;
 	}
 
-	reverse_listint(&dup);
-
-	while (dup && temp)
+	while ((*head)->next != NULL)
 	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next;
-			temp = temp->next;
-		}
-		else
-			return (0);
+		*head = (*head)->next;
 	}
 
-	if (!dup)
+	if (ptr == *head)
 		return (1);
 
 	return (0);
 }
-
