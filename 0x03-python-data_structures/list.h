5c2e385044dbb1fd7f06d6fc4eaa8c9eb9f172bd
tony <ebunifeoluwaadenuga@gmail.com>
2023-03-14 02:47:25 +0000

    aaa

--- 0x03-python-data_structures/list.h
+++ 0x03-python-data_structures/list.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <stddef.h>
 
 /**
@@ -9,7 +12,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -21,6 +24,8 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
+
